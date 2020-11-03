1111ba495213fc8be8ad7ccb177983efabcd0566
denisavilamontini <31044399+denisavilamontini@users.noreply.github.com>
2020-11-03 09:24:36 -0300

    Biblioteca de Pacotes Iniciais
    Biblioteca contendo um conjunto de Pacotes básicos em linguagem Python.

---
+++ __init__.py
@@ -0,0 +1,5 @@
+
+__init__.py
+from exemplo1.py import exemplo1.py
+from exemplo2.py import exemplo2.py
+from exemplo3.py import exemplo3.py
---
+++ exemplo2.py
@@ -0,0 +1,4 @@
+num1 = int(input("Digite um valor: "))
+num2 = int(input("Digite um valor: "))
+soma = num1 + num2/2
+print("Resultado: ", soma)
