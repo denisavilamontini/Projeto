37f4e2fe82749585c91a8e84887530840b245753
denisavilamontini <31044399+denisavilamontini@users.noreply.github.com>
2020-11-03 10:44:32 -0300

    Setup
    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(name=' package_name',
+      version='0.1',
+      url='https://github.com/denisavilamontini/Projeto',
+      license='GPL-3.0 License',
+      author='Denis Avila Montini',
+      author_email='denisavilamontini@yahoo.com.br',
+      description='Add static script_dir() method to Path',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
